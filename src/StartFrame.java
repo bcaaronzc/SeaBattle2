1433777bd2399c7782470248ff8afcf1c213ddec
Aaron <panyitao1997@hotmail.com>
2017-05-04 11:23:31 +0800

    Add three buttons.

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -1,12 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class StartFrame extends JFrame{
+public class StartFrame extends JFrame implements ActionListener{
 	JButton start, highScore, quit;
 	
 	public StartFrame(){
@@ -21,6 +23,10 @@ public class StartFrame extends JFrame{
 		highScore = new JButton("High Score");
 		quit = new JButton("Quit");
 		
+		start.addActionListener(this);
+		highScore.addActionListener(this);
+		quit.addActionListener(this);
+		
 		buttonPan.add(highScore);
 		buttonPan.add(start);
 		buttonPan.add(quit);
@@ -28,6 +34,18 @@ public class StartFrame extends JFrame{
 		this.add(buttonPan, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if (e.getActionCommand() == "Start"){
+			System.out.println("Start");
+		}
+		if (e.getActionCommand() == "High Score"){
+			System.out.println("HighScore");
+		}
+		if (e.getActionCommand() == "Quit"){
+			System.out.println("Quit");
+		}
 	}
 
 	public static void main(String[] args) {
