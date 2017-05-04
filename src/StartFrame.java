bf1a7953a8b1ef6e5174b050634292fad63ad9c6
Aaron <panyitao1997@hotmail.com>
2017-05-04 10:53:44 +0800

    Start over. Add some images that may be useful in the future.

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -1,55 +1,31 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class StartFrame extends JFrame{
 	JButton start, highScore, quit;
-	JLabel background;
 	
 	public StartFrame(){
 		this.setBounds(200, 200, 600, 400);
 		this.setBackground(new Color(255, 255, 255));
 		this.getContentPane().setBackground(Color.white);
 		
-		JPanel imagePanel = new JPanel();
-		ImageIcon backgroundIcon = new ImageIcon("src/Image/StartFrameBoat.png");
-		Image backgroundImage = backgroundIcon.getImage().getScaledInstance(imagePanel.getWidth(),imagePanel.getHeight(),backgroundIcon.getImage().SCALE_DEFAULT);  
-		ImageIcon finalIcon = new ImageIcon(backgroundImage);
-		background = new JLabel();
-		background.setIcon(finalIcon);
-		this.add(background, BorderLayout.CENTER);
+		JPanel buttonPan = new JPanel();
+		buttonPan.setLayout(new GridLayout(1, 3));
 		
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(1, 3));
-		Font buttonFont = new Font("Arial", Font.BOLD, 24);
-		
-		start = new JButton();
-		start.setIcon(new ImageIcon("src/Image/StartFrameBoat.png"));
-		start.setBackground(Color.WHITE);
+		start = new JButton("Start");
 		highScore = new JButton("High Score");
 		quit = new JButton("Quit");
 		
-		start.setFont(buttonFont);
-		highScore.setFont(buttonFont);
-		quit.setFont(buttonFont);
+		buttonPan.add(highScore);
+		buttonPan.add(start);
+		buttonPan.add(quit);
 		
-		buttonPanel.add(highScore);
-		buttonPanel.add(start);
-		buttonPanel.add(quit);
-		
-		start.setForeground(Color.BLUE);
-		highScore.setForeground(Color.BLUE);
-		quit.setForeground(Color.BLUE);
-		
-		this.add(buttonPanel, BorderLayout.SOUTH);
+		this.add(buttonPan, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 	}
