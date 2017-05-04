e1575e671491d56873490f7e9da7fa92888e88c0
Aaron <panyitao1997@hotmail.com>
2017-05-05 01:26:57 +0800

    Add something in GameBoard class, don't know if they are useful.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,6 +1,13 @@
 
 public class GameBoard {
+	static int BOARDROW = 7;
+	static int BOARDCOL = 7;
+	static int shipNum = 3;
 
+	int[][] gameBoard = new int[BOARDROW][BOARDCOL];
+	
+	BattleShip[] battleShips = new BattleShip[shipNum];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
