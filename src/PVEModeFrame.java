260876a6e7564b2ecd4e4b2a9c3d0eed85dddd80
Aaron <panyitao1997@hotmail.com>
2017-05-27 15:48:12 +0800

    Add buttons for PVE mode.

--- src/PVEModeFrame.java
+++ src/PVEModeFrame.java
@@ -1,9 +1,70 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PVEModeFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class PVEModeFrame extends JFrame implements ActionListener{
+	GameBoard computerBoard, playerBoard;
+	JButton[][] computerButtons, playerButtons;
+	
+	// 构造函数
+	public PVEModeFrame(){
+		computerBoard = new GameBoard();
+		playerBoard = new GameBoard();
+		computerButtons = new JButton[computerBoard.getRowNum()][computerBoard.getColNum()];
+		playerButtons = new JButton[playerBoard.getRowNum()][playerBoard.getColNum()];
+		
+		this.setTitle("SeaBattle2");
+		this.setSize(240 + 2 * 80 * computerBoard.getRowNum(), 80 * computerBoard.getColNum());
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		addButtons();
+		
+		this.setVisible(true);
+	}
+	
+	// 添加按钮(for the constructor)
+	public void addButtons(){
+		JPanel computerButtonPanel = new JPanel();
+		JPanel playerButtonPanel = new JPanel();
+		computerButtonPanel.setLayout(new GridLayout(computerBoard.getRowNum(), computerBoard.getColNum()));
+		playerButtonPanel.setLayout(new GridLayout(playerBoard.getRowNum(), playerBoard.getColNum()));
+		for (int row = 0; row < computerBoard.getRowNum(); row++){
+			for (int col = 0; col < computerBoard.getColNum(); col++){
+				computerButtons[row][col] = new JButton("");
+				computerButtons[row][col].setBackground(new Color(162, 230, 255));
+				computerButtons[row][col].addActionListener(this);
+				computerButtons[row][col].setPreferredSize(new Dimension(80, 80));
+				computerButtonPanel.add(computerButtons[row][col]);
+			}
+		}
+		for (int row = 0; row < playerBoard.getRowNum(); row++){
+			for (int col = 0; col < playerBoard.getColNum(); col++){
+				playerButtons[row][col] = new JButton("");
+				playerButtons[row][col].setBackground(new Color(162, 230, 255));;
+				playerButtons[row][col].addActionListener(this);
+				playerButtons[row][col].setPreferredSize(new Dimension(80, 80));
+				playerButtonPanel.add(playerButtons[row][col]);
+			}
+		}
+		this.add(computerButtonPanel, BorderLayout.WEST);
+		this.add(playerButtonPanel, BorderLayout.EAST);
+	}
+
+	// 监听方法
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PVEModeFrame pveMode = new PVEModeFrame();
 	}
 
 }
